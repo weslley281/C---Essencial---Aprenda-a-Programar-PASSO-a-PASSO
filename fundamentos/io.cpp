ed80ca701d34e3be4296bc68046da5a9270c84a0
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-08-19 05:35:28 -0400

    a

---
+++ fundamentos/io.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Olá\n";
+    cout << 2.14 << endl;
+
+    int x = 3;
+
+    cout << x << endl;
+
+    string name;
+    cout << "Escreva o seu nome: ";
+
+    cin >> name;
+
+    cout << name << endl;
+
+    return 0;
+}
---
+++ fundamentos/std.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    cout << "Olá mundo" << endl;
+
+    return 0;
+}
