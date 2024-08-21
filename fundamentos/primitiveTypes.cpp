847db8376ab2703b26f7d3bfbb26fb52d74d4c3e
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-08-21 09:13:22 -0400

    a

---
+++ fundamentos/primitiveTypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool isAdmin = 1;
+    cout << isAdmin << endl;
+
+    char symbol = '#';
+    cout << symbol << endl;   
+
+    unsigned short int age = 28;
+    cout << age << endl;
+
+    float pi = 3.14;
+    cout << pi << endl;
+
+    double price = 39.99;
+    cout << price << endl;
+
+    long int quant = 56895364256;
+    cout << quant << endl;
+
+    return 0;
+}
