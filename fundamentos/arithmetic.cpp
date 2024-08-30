706e3aad3fd6626271ea3eb0755640826df898cb
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-08-30 10:20:34 -0400

    a

---
+++ fundamentos/arithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << 2 + 2 << endl;
+    cout << 2 - 2 << endl;
+    cout << 2 * 2 << endl;
+    cout << 2 / 2 << endl;
+    cout << 2 % 2 << endl;
+
+    cout << (155 * 2) / 100 << endl;
+
+    return 0;
+}
