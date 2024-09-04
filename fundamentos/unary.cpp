b2720714e256e62e5ef85ef061418926b819d126
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-09-04 15:05:00 -0400

    a

--- fundamentos/unary.cpp
+++ fundamentos/unary.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
-#
+#include <cstdio>
+
+int main() {
+    int x = 2;
+    int y = 1;
+
+    x++;
+    ++y;
+    printf("%d %d\n", x, y);
+
+    x--;
+    --y;
+    printf("%d %d\n", x, y);
+
+    return 0;
+}
